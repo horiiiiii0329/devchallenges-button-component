570e21a6266ccae500d9abaa572e7dff01bd0e54
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-08-22 23:00:26 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="px-32 py-20 flex">
+      <h1 className="font-sans text-2xl text-gray-400">Buttons</h1>
+    </div>
+  );
 }
 
 export default App;
